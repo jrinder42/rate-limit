6296e8b743c39eb6406cf3bd53b047772eea01c3
Jordan Rinder <jrinder42@users.noreply.github.com>
2025-08-08 21:55:38 -0400

    code formatting

--- rate_limit/base.py
+++ rate_limit/base.py
@@ -3,13 +3,14 @@
 from __future__ import annotations
 
 from types import TracebackType
-from typing import Protocol, Any
+from typing import Any, Protocol
 
 
 class SyncRateLimit(Protocol):
     """Synchronous Rate Limit Protocol"""
 
-    def __init__(self, config: Any) -> None:...
+    def __init__(self, config: Any) -> None:
+        ...
 
     def acquire(self) -> None:
         """Acquire an item from the rate limit. This method should block until a token is available"""
@@ -37,7 +38,8 @@ class SyncRateLimit(Protocol):
 class AsyncRateLimit(Protocol):
     """Asynchronous Rate Limit Protocol"""
 
-    def __init__(self, config: Any, max_concurrent: int | None = None) -> None:...
+    def __init__(self, config: Any, max_concurrent: int | None = None) -> None:
+        ...
 
     def acquire(self) -> None:
         """Acquire an item from the rate limit. This method should block until a token is available"""
