ea021940baa34fff4dec1734b3dca4ce520c35ce
Jordan Rinder <jrinder42@users.noreply.github.com>
2025-11-16 14:16:03 -0500

    remove incorrect return statement

--- limitor/base.py
+++ limitor/base.py
@@ -39,7 +39,6 @@ class SyncRateLimit(Protocol):
         Returns:
             An instance of the rate limit context manager
         """
-        return self
 
     def __exit__(self, exc_type: type[BaseException], exc_val: BaseException, exc_tb: TracebackType) -> None:
         """Exit the context manager, releasing any resources if necessary
