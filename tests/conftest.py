15d5ec428c2c568bc16a6504f1ec65457b3bb55d
Jordan Rinder <jrinder42@users.noreply.github.com>
2025-11-16 22:23:42 -0500

    remove comment

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,6 @@ from limitor.configs import BucketConfig
 
 
 @pytest.fixture
-def bucket_config() -> BucketConfig:  # should be a global fixture in conftest.py
+def bucket_config() -> BucketConfig:
     """Fixture that provides a BucketConfig with capacity=2, seconds=0.2 for tests"""
     return BucketConfig(capacity=2, seconds=0.2)
